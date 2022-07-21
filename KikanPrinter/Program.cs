1863411781c2c9c14eed04984ac41425fa9b0f88
nghe.huavan <nghe.huavan@gmail.com>
2022-07-21 12:35:29 +0700

    .

--- KikanPrinter/Program.cs
+++ KikanPrinter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
 using System.Windows.Forms;
 using KikanPrinter.Web;
 
@@ -6,17 +8,36 @@ namespace KikanPrinter
 {
     static class Program
     {
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            WebHost.Host.Start();
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            if (!IsAdministrator())
+            {
+                // Restart and run as admin
+                var exeName = Process.GetCurrentProcess().MainModule?.FileName;
+                var startInfo = new ProcessStartInfo(exeName) { Verb = "runas", Arguments = "restart" };
+                Process.Start(startInfo);
+                Application.Exit();
+            }
+            else
+            {
+                WebHost.Host.Start();
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Main());
+            }
         }
     }
 }
